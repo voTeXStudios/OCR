2630a4bdc11853debf59d6b53a2d7f5b631749ed
Sc0tty <63871153+TALLEC-Scott@users.noreply.github.com>
2020-12-12 14:24:02 +0100

    Update GaussianBlur.h

--- OCR/GaussianBlur.h
+++ OCR/GaussianBlur.h
@@ -1,8 +1,18 @@
-#ifndef _GAUSSIAN_BLUR_
-#define _GAUSSIAN_BLUR_
+#ifndef CROP_IMAGE
+#define CROP_IMAGE
 
 #include<stdlib.h>
 #include"SDL/SDL.h"
-void Convolution(SDL_Surface *img);
+
+//int CalcMinFromLeft(SDL_Surface *image);
+//int CalcMinFromRight(SDL_Surface *image);
+//int CalcMinFromTop(SDL_Surface *image);
+//int CalcMinFromBot(SDL_Surface *image);
+//SDL_Surface* CropLeft(SDL_Surface *img);
+//SDL_Surface* CropRight(SDL_Surface *img);
+//SDL_Surface* CropTop(SDL_Surface *img);
+//SDL_Surface* CropBottom(SDL_Surface *img);
+SDL_Surface* ChipTheEdges(SDL_Surface *img);
+
 
 #endif
